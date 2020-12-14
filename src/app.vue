be07ed8c5cc6fa835cb843bb71d3a531ecec2714
xiangyang <1256599321@qq.com>
2020-12-14 18:48:44 +0800

    评论

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,45 @@
 <template>
-  <div>
-      <MyButton></MyButton>
-  </div>
+    <!-- <MyButton></MyButton> -->   
+    <div class="container">
+        <Header></Header>
+        <Add :addComment="addComment"></Add>
+        <List :coms="comments" :deleteComment="deleteComment"></List>
+    </div> 
 </template>
 
 <script>
 // import MyButton from '@/components/MyButton'
-import MyButton from './components/MyButton/MyButton.vue'
+// import MyButton from './components/MyButton/MyButton.vue'
+import Header from './components/Header'
+import Add from './components/Add'
+import List from './components/List'
+
 
 export default {
   name: '',
   components:{
-      MyButton:MyButton
-    
-  }
+      // MyButton:MyButton
+      Header:Header,
+      Add,
+      List,
+  },
+  data(){
+    return {
+      comments:[
+        {id:1,content:'vue不错',username:'刘向阳'},
+        {id:2,content:'诶呦,不错哦',username:'谢可寅'},
+        {id:3,content:'你敢相信吗? 真不错!',username:'韦俊霞'},
+      ]
+    }
+  },
+   methods: {
+    addComment(comment){
+      this.comments.unshift(comment)
+    },
+    deleteComment(index){
+      this.comments.splice(index,1)
+    }
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
-import App from './app.vue'
+import App from './App.vue'
 
 new Vue({
-    el:"#app",
-    components:{
-        App
-    },
-    
+    el:"#app",    
     render:h=>h(App)
 })
